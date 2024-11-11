da749bbe45af7a071dae5661eab887daa149caf3
Dekbooo <143566791+Dekbooo@users.noreply.github.com>
2024-11-10 21:17:32 -0600

    Creacion de Practica Ant

---
+++ src/poop1_ant/POOP1_ANT.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1_ant;
+
+/**
+ *
+ * @author Derek
+ */
+public class POOP1_ANT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo con Ant!");
+    }
+}
