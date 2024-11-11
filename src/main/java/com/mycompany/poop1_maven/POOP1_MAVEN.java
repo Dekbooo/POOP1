dbccd1bc793837f3846c5bf227eadfaef3127bb3
Dekbooo <143566791+Dekbooo@users.noreply.github.com>
2024-11-10 21:16:56 -0600

    Creacion de Practica MAVEN

---
+++ src/main/java/com/mycompany/poop1_maven/POOP1_MAVEN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author Derek
+ */
+public class POOP1_MAVEN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
